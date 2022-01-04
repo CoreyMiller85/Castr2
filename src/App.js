2c947d165a58a1f353c54e7be542320646f5b23a
Corey Miller <coreymiller454@gmail.com>
2022-01-04 01:14:41 -0500

    add all set names for filters

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Header from './components/Header/Header';
-import CardContainer from './components/CardContainer/CardContainer';
-import NewHeader from './components/NewHeader/NewHeader';
 import { Routes, Route } from 'react-router';
-import { BrowserRouter } from 'react-router-dom';
 import SingleCardPage from './components/SingleCardPage/SingleCardPage';
 import Menu from './components/Menu/Menu';
+
 function App() {
   return (
     <div className='App'>
